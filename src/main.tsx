e9e771ddea88306994f848be9a5bb8f57d200a62
vspeed21 <vtorresquintanilla0206@gmail.com>
2022-12-28 21:28:11 -0600

    create router and signup route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AuthLayout from './layout/AuthLayout';
+import SignUp from './pages/auth/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: '/auth',
+    element: <AuthLayout/>,
+    children: [
+      {
+        path: 'signup',
+        element: <SignUp/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
