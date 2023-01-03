5e69eb58f3ba01383affa40094f26bb0c9dc7eaa
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-02 20:46:48 -0600

    types for context props created

---
+++ src/context/types.ts
@@ -0,0 +1,12 @@
+export type Admin = {
+  _id: string,
+  name: string,
+  email: string,
+  password: string,
+  token: string | null,
+  confirmed: boolean
+}
+
+export type ContextAuthProps = {
+  auth: Admin,
+}
