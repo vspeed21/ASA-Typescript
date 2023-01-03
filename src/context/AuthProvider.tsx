604d849c5f9579439b8b05907d1cd18f6530b71c
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-02 21:23:34 -0600

    fetching to api to validate user and show admin panel

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
+import adminClient from "../config/adminClient";
 
 export const AuthContext = createContext({});
 
@@ -7,10 +8,41 @@ interface Props {
 }
 
 function AuthProvider({children}: Props) {
+  const [auth, setAuth] = useState({});
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const token = localStorage.getItem('tokenasa');
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${token}`
+    }
+  }
+
+  useEffect(() => {
+    validateUser();
+    async function validateUser() {
+      if(!token) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const { data } = await adminClient('/profile/profileList', config);
+        setAuth(data);
+      } catch (error) {
+        console.log(error);
+        setAuth({});
+      }
+      setLoading(false)
+    }
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
-        hola: 'hola desde context'
+        auth,
+        loading,
       }}
     >
       {children}
