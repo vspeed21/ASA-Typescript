db26b9e4af0f3a9abefb102c27b6078ad7f6e420
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-03 21:51:12 -0600

    context prop added on hooks

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthProvider";
+import { ContextAuthProps } from "../context/types";
 
 function useAuth() {
-  return useContext(AuthContext);
+  return useContext(AuthContext) as ContextAuthProps;
 }
 
 export default useAuth
--- src/hooks/useProfile.tsx
+++ src/hooks/useProfile.tsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { ProfileContext } from "../context/ProfileProvider";
+import { ContextProfileProps } from "../context/types";
 
 function useProfile() {
-  return useContext(ProfileContext);
+  return useContext(ProfileContext) as ContextProfileProps;
 }
 
 export default useProfile
