d697726a11538caec2c09d9d71297785fb6aa6ca
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-04 23:07:20 -0600

    Spinner created

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,18 @@
+import '../styles/Spinner.css';
+
+function Spinner() {
+  return (
+    <div className='flex justify-center'>
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
