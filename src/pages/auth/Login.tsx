b67017202d8dc6aee6985d95b0de0dc0e38f6229
vspeed21 <vtorresquintanilla0206@gmail.com>
2023-01-06 15:16:46 -0600

    redirecting with window.location instead useNavigate hook on Login

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -58,7 +58,7 @@ function Login() {
       setValid(false)
       setValidPassword(false);
 
-      navigate('/')
+      window.location.href = '/'
       setShowSpinner(false)
 
     } catch (error: any) {
